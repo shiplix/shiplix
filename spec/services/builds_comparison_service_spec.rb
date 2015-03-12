341497dc2823b2f352820e32791351011cbbf2c3
bibendi <bibendi@bk.ru>
2015-03-12 11:19:59 +0500

    Generate source files changsets after push build
    TG-3

--- spec/services/builds_comparison_service_spec.rb
+++ spec/services/builds_comparison_service_spec.rb
@@ -6,7 +6,7 @@ describe BuildsComparisonService do
   let(:source) { create :push, branch: branch }
   let(:service) { described_class.new(target, source) }
 
-  context 'when no klasses' do
+  context 'when repo empty' do
     before { service.call }
     it { expect(target.changesets).to be_empty }
   end
@@ -16,42 +16,85 @@ describe BuildsComparisonService do
     it { expect { service.call }.to raise_error }
   end
 
-  context 'when has klasses with same rating' do
-    before do
-      create :klass, name: 'Name', build: target, rating: 1
-      create :klass, name: 'Name', build: source, rating: 1
-      service.call
+  describe 'Klass' do
+    context 'when has klasses with same rating' do
+      before do
+        create :klass, name: 'Name', build: target, rating: 1
+        create :klass, name: 'Name', build: source, rating: 1
+        service.call
+      end
+
+      it { expect(target.changesets).to be_empty }
     end
 
-    it { expect(target.changesets).to be_empty }
+    context 'when has klasses with different rating' do
+      before do
+        create :klass, name: 'Name', build: target, rating: 1
+        create :klass, name: 'Name', build: source, rating: 2
+        service.call
+      end
+
+      let(:changeset) { target.changesets.first }
+
+      Then { expect(target.changesets.size).to eq 1 }
+      And { expect(changeset.subject).to eq target.klasses.first }
+      And { expect(changeset.rating).to eq 1 }
+      And { expect(changeset.prev_rating).to eq 2 }
+    end
+
+    context 'when adds new klasses' do
+      before do
+        create :klass, name: 'Name', build: target, rating: 1
+        service.call
+      end
+
+      let(:changeset) { target.changesets.first }
+
+      Then { expect(target.changesets.size).to eq 1 }
+      And { expect(changeset.subject).to eq target.klasses.first }
+      And { expect(changeset.rating).to eq 1 }
+      And { expect(changeset.prev_rating).to be_nil }
+    end
   end
 
-  context 'when has klasses with different rating' do
-    before do
-      create :klass, name: 'Name', build: target, rating: 1
-      create :klass, name: 'Name', build: source, rating: 2
-      service.call
+  describe 'SourceFile' do
+    context 'when has source files with same rating' do
+      before do
+        create :source_file, path: 'path/name.rb', build: target, rating: 1
+        create :source_file, path: 'path/name.rb', build: source, rating: 1
+        service.call
+      end
+
+      it { expect(target.changesets).to be_empty }
     end
 
-    let(:changeset) { target.changesets.first }
+    context 'when has source files with different rating' do
+      before do
+        create :source_file, path: 'path/name.rb', build: target, rating: 1
+        create :source_file, path: 'path/name.rb', build: source, rating: 2
+        service.call
+      end
 
-    Then { expect(target.changesets.size).to eq 1 }
-    And { expect(changeset.subject).to eq target.klasses.first }
-    And { expect(changeset.rating).to eq 1 }
-    And { expect(changeset.prev_rating).to eq 2 }
-  end
+      let(:changeset) { target.changesets.first }
 
-  context 'when adds new klasses' do
-    before do
-      create :klass, name: 'Name', build: target, rating: 1
-      service.call
+      Then { expect(target.changesets.size).to eq 1 }
+      And { expect(changeset.subject).to eq target.source_files.first }
+      And { expect(changeset.rating).to eq 1 }
+      And { expect(changeset.prev_rating).to eq 2 }
     end
 
-    let(:changeset) { target.changesets.first }
+    context 'when adds new klasses' do
+      before do
+        create :source_file, path: 'path/name.rb', build: target, rating: 1
+        service.call
+      end
 
-    Then { expect(target.changesets.size).to eq 1 }
-    And { expect(changeset.subject).to eq target.klasses.first }
-    And { expect(changeset.rating).to eq 1 }
-    And { expect(changeset.prev_rating).to be_nil }
+      let(:changeset) { target.changesets.first }
+
+      Then { expect(target.changesets.size).to eq 1 }
+      And { expect(changeset.subject).to eq target.source_files.first }
+      And { expect(changeset.rating).to eq 1 }
+      And { expect(changeset.prev_rating).to be_nil }
+    end
   end
 end
