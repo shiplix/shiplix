e1606655b5e9c2131cf0872c668cb516e459d928
Michail Merkushin <merkushin.m.s@gmail.com>
2015-10-01 09:53:01 +0500

    sets default rating to zero

--- app/models/concerns/ratingable.rb
+++ app/models/concerns/ratingable.rb
@@ -4,7 +4,7 @@ module Ratingable
   included do
     validates :rating_smells_count, numericality: {only_integer: true, greater_than_or_equal_to: 0}
     validates :total_rating, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-    validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+    validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 
     before_save :compute_rating
   end
