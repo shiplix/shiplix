5d9ffda9ec5f3fc5632bcd7b11260be8a73b5d51
Semyon Pupkov <semen.pupkov@gmail.com>
2015-11-11 00:53:05 +0500

    Remove spec for models
    For flog we don't use rating method.
    For reek now we don't use a column to save trait.

--- spec/models/smells/flog_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Smells::Flog do
-  describe '#rating' do
-    let(:smell) { build_stubbed :smell_flog, :with_class, score: 100_500 }
-
-    it { expect(smell.rating).to eq 5 }
-  end
-end
